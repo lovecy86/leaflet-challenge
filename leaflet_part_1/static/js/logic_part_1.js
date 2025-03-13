cb0a512a848e40cde2b9e6dbf14921684d742a8a
lovecy86 <lovecy.ssec@gmail.com>
2025-03-12 21:18:17 -0400

    included depth in the popup

--- leaflet_part_1/static/js/logic_part_1.js
+++ leaflet_part_1/static/js/logic_part_1.js
@@ -83,7 +83,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     style: styleInfo,
     // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
     
     }
   // OPTIONAL: Step 2
